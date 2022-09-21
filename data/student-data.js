8c338dd59854ae6bf3b5e44a9ccaf5399afa1ae0
davidv <davidvazquezgit@gmail.com>
2022-09-21 13:11:19 -0700

    add boiler plate to index.ejs under students dir, mkdir a data dir, touch a student-data.js, add data to file

---
+++ data/student-data.js
@@ -0,0 +1,9 @@
+const students = [
+  {name: 'Fred llama', passed: true, _id: 125223},
+  {name: 'Sleep Stars', passed: false, _id: 127904},
+  {name: 'Buy milk', passed: false, _id: 139608},
+]
+
+export { 
+	students
+}
