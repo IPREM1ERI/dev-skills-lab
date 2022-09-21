781972c7ad15f20959e5eccc20f4711096eb1b66
davidv <davidvazquezgit@gmail.com>
2022-09-21 13:23:41 -0700

    rename user.js in routes dir to students.js, refactor users import route to students, refactor where user is mounted to students

--- routes/students.js
+++ routes/students.js
@@ -1,10 +1,17 @@
 import { Router } from 'express'
+import { students } from '../data/student-data'
 
 const router = Router()
 
 /* GET users listing. */
 router.get('/', function(req, res) {
   res.send('respond with a resource')
+})
+
+router.get('/', function(req, res) {
+  res.render('students/index', {
+    students: students
+  })
 })
 
 export {
